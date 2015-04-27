2428f39625fc778428d33ac2d73d131078527df3
Dan Ellis <dan@danellis.me>
2015-04-26 21:06:40 -0400

    Use requests library to fetch /robots.txt

--- cosmo/crawler.py
+++ cosmo/crawler.py
@@ -1,6 +1,8 @@
 import sys
 from urllib.parse import urlparse, urlunparse
 from urllib.robotparser import RobotFileParser
+import requests
+from cosmo.version import version
 
 class Crawler(object):
     def __init__(self, database, fetcher, analyzer, verbose=False):
@@ -52,7 +54,23 @@ class Crawler(object):
         return (parsed1.scheme, parsed1.netloc) == (parsed2.scheme, parsed2.netloc)
 
     def load_robots_file(self, url):
+        """Load the /robots.txt file for the given URL by reusing the scheme
+        and authority parts.
+
+        :param url: The URL from which to take the scheme and authority parts.
+        :returns: None
+        """
         parsed = urlparse(url)
         robots_url = urlunparse((parsed.scheme, parsed.netloc, '/robots.txt', '', '', ''))
-        self.robot_parser.set_url(robots_url)
-        self.robot_parser.read()
+
+        # Load the robots.txt file using the requests library, because we need
+        # to specify the User-Agent header. I noticed on a CloudFlare-fronted
+        # site that it returns a 403 for /robots.txt if the the user agent is
+        # Python-urllib, but 200 if it's Cosmo.
+        response = requests.get(robots_url, headers={'User-Agent': 'Cosmo/{}'.format(version)})
+        if response.status_code in (401, 403):
+            self.robot_parser.disallow_all = True
+        elif response.status_code >= 400:
+            self.robot_parser.allow_all = True
+        else:
+            self.robot_parser.parse(response.text)
